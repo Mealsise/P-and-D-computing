1d8c12b375b377c7abe6d4ec1b3fc6add29387cd
Mealsise <Tyler@roe-smith.com>
2025-04-04 11:16:55 +1030

    Implemented partial check_rank
    I dont like this
    
    ill change it to something where if it needs to self right it will than early return for 0 then receive left return output

--- numbers1.c
+++ numbers1.c
@@ -13,6 +13,30 @@ inline int neighbour_right(int my_rank, int num_ranks) {
     return (my_rank + 1) % num_ranks;
 }
 
+bool check_rank(int my_value, int my_rank, int num_ranks) {
+    if (my_rank == 0)
+    {
+        // Send right
+        return true;
+    }
+    else if (my_rank == num_ranks - 1)
+    {
+        int right = my_value;
+        int left;
+        // Receive left
+        return left < right;
+    }
+    else
+    {
+        int right = my_value;
+        int left;
+        // Receive left
+        // Send right
+        return left < right;
+    }
+}
+
+
 int main(void) {
     // Initialize
     int my_rank, num_ranks;
@@ -22,17 +46,19 @@ int main(void) {
 
     // Input
 //TODO Change over from FAKE INPUT
-    int values[6] = {5, 7, 9, 1, 13, 14}; // Hard coded to only be 6 as my pc can only run upto 6 processes `mpirun -np 6 ./numbers1`
+    // Hard coded to only be 6 as my pc can only run upto 6 processes `mpirun -np 6 ./numbers1`
+    // 5 would be ample given assignment constraints but I wanted for testing
+    int values[6] = {5, 7, 9, 1, 13, 16}; 
     int my_value = values[my_rank];
-    printf("Rank %d: My number is %d\n", my_rank, my_value);
+    printf("Rank %d: My number is %d\n", my_rank, my_value);    //! Temporary
 
     // Check
-    bool correct = false;
+    bool correct = check_rank(my_value, my_rank, num_ranks);
     if (correct)
     {
-        printf("Process %d has at least one number out of order.\n", my_rank);
+        printf("Process %d has now finished.\n", my_rank);
     }
-    printf("Process %d has now finished.\n", my_rank);
+    printf("Process %d has at least one number out of order.\n", my_rank);
 
     // Finalize
     MPI_Finalize();
