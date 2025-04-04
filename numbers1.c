caf2bfd4095c576ea8f99524108fc8d915fd0998
Mealsise <Tyler@roe-smith.com>
2025-04-04 11:49:54 +1030

    Clean and Comment
    Cleaning and Commenting

--- numbers1.c
+++ numbers1.c
@@ -3,22 +3,30 @@
 #include <stdio.h>
 #include <mpi.h>
 
-// inline function to get the rank of a ranks left neighbour
+// Returns the rank of the left neighbour in the ring topology.
+// Uses modulo to wrap around for rank 0, making the topology circular.
+// Example: if my_rank = 0 and num_ranks = 5 → left = 4
 static inline int get_left_rank(int my_rank, int num_ranks) {
     return (my_rank - 1) % num_ranks;
 }
 
-// inline function to get the rank of a ranks right neighbour
+// Returns the rank of the right neighbour in the ring topology.
+// Uses modulo to wrap around for the last rank.
+// Example: if my_rank = 4 and num_ranks = 5 → right = 0
 static inline int get_right_rank(int my_rank, int num_ranks) {
     return (my_rank + 1) % num_ranks;
 }
 
+// Sends this process's value to its right neighbour in the ring.
+// This function uses point-to-point MPI_Send communication.
 static inline void send_right(int my_value, int my_rank, int num_ranks)
 {
     int right_rank = get_right_rank(my_rank, num_ranks);
     MPI_Send(&my_value, 1, MPI_INT, right_rank, 0, MPI_COMM_WORLD);
 }
 
+// Receives an integer value from the left neighbour in the ring.
+// This function uses point-to-point MPI_Recv communication.
 static inline int receive_left(int my_rank, int num_ranks)
 {
     int left_value;
@@ -27,50 +35,64 @@ static inline int receive_left(int my_rank, int num_ranks)
     return left_value;
 }
 
-
+// Determines if the current process's value is in correct order
+// relative to its left neighbour in a ring topology.
+// Returns true if the value is in order (i.e., not less than the left neighbour's value),
+// or if this is rank 0, which has no left neighbour.
+//
+// In this ring communication model:
+// - Each process (except the last) sends its value to the right neighbour
+// - Each process (except rank 0) receives a value from its left neighbour
+// - A value is considered out of order if it is less than its left neighbour's value
 bool is_in_order(int my_value, int my_rank, int num_ranks) {
-    // send right unless last
+    // Send this process’s value to the right neighbour.
+    // Only needed if not the last rank (to avoid wrapping unnecessarily).
     if (my_rank != num_ranks - 1)
     {
         send_right(my_value, my_rank, num_ranks);
     }
 
-    // Rank 0 has no left neighbour and thus has no need to check
-    //   Thus defaults to passing
+    // Rank 0 has no left neighbour to compare against.
+    // By definition, it is always considered "in order".
     if (my_rank == 0)
     {
         return true;
     }
 
-    // grab from the left and compare
+    // Receive the left neighbour’s value and compare.
+    // If our value is greater than or equal to the left neighbour's, it is in order.
     int left_value = receive_left(my_rank, num_ranks);
     return left_value < my_value; // true = in order
 }
     
 
 int main(void) {
-    // Initialize
+    // Initialize MPI environment and get process details.
     int my_rank, num_ranks;
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+    MPI_Init(NULL, NULL);                              // Start the MPI environment
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);           // Get the rank of this process
+    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);         // Get the total number of processes
 
-    // Input
-//TODO Change over from FAKE INPUT
-    // Hard coded to only be 6 as my pc can only run upto 6 processes `mpirun -np 6 ./numbers1`
-    // 5 would be ample given assignment constraints but I wanted for testing
+    // Provide input values for each rank (temporary setup).
+    // TODO: Real implementation will include external input
+    // For testing, I hardcode an array and each process selects its value by rank.
+    // This assumes no more than 6 processes are launched (`mpirun -np 6 ./numbers1`).
+    // Running with more may cause out-of-bounds access (undefined behaviour).
     int values[6] = {5, 7, 9, 1, 13, 16}; 
     int my_value = values[my_rank];
-    printf("Rank %d: My number is %d\n", my_rank, my_value);    //! Temporary
+    printf("Rank %d: My number is %d\n", my_rank, my_value);  // Temporary debug output
 
-    // Check
+    // Check if this process's value is in correct order relative to the left neighbour.
+    // If not, print a message indicating that it's out of order.
     if (!is_in_order(my_value, my_rank, num_ranks))
     {
         printf("Process %d has at least one number out of order.\n", my_rank);
     }
+
+    // All processes print a termination message as required.
     printf("Process %d has now finished.\n", my_rank);
 
-    // Finalize
+    // Finalize MPI environment.
     MPI_Finalize();
 
     return 0;
